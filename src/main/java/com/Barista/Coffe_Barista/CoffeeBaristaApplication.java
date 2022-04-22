6760f710bfdbd979369a6f0d3af39d9769373464
adi0212 <74491488+adi0212@users.noreply.github.com>
2022-04-22 14:30:05 +0300

    Starting a project in spring boot in which I want to create an application for barista in which you can generate a price for a caffe with different elements

---
+++ src/main/java/com/Barista/Coffe_Barista/CoffeeBaristaApplication.java
@@ -0,0 +1,13 @@
+package com.Barista.Coffe_Barista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeBaristaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeBaristaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Barista/Coffe_Barista/CoffeeBaristaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Barista.Coffe_Barista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeBaristaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
